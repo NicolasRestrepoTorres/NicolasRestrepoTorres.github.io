7a566a0a96ae418e9c4d232fbdc54541ff39f0f1
Nicolas Restrepo Torres <nrestrepot@unal.edu.co>
2023-06-08 15:28:30 +0300

    Config chnged

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 
-  publicPath: process.env.NODE_ENV === "production" ? "/NicolasRestrepoTorres/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "NicolasRestrepoTorres" : "/",
   "transpileDependencies": [
     "vuetify"
   ],
